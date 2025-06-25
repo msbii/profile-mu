6ebb88989365c868b3a07f6123fc993805bb562f
Syafri Abidin <94607269+msbii@users.noreply.github.com>
2025-06-26 06:47:07 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 
-class post extends Model
+class Post extends Model
 {
     use HasFactory, Sluggable;
 
