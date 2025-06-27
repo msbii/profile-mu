17b4d5bf1153694f7d67ee5c3c7571f263ea4569
Msbii <abidinsyafri51@gmail.com>
2025-06-27 23:08:23 +0700

    -

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -51,8 +51,15 @@ class DashboardPostController extends Controller
             'body' => 'required'
         ]);
         // check jika img tidak ada maka unsplash
+        // if ($request->file('image')) {
+        //     $validateData['image'] = $request->file('image')->store('public/post-images');
+        // }
+
+        // Simpan file gambar
         if ($request->file('image')) {
-            $validateData['image'] = $request->file('image')->store('public/post-images');
+            $path = $request->file('image')->store('public/post-images');
+            // Hapus 'public/' agar yang disimpan hanya 'post-images/namafile.png'
+            $validateData['image'] = str_replace('public/', '', $path);
         }
 
         // Menyimpan data ke dalamm post
@@ -112,12 +119,23 @@ class DashboardPostController extends Controller
 
         
         // check jika img tidak ada maka unsplash
+        // if ($request->file('image')) {
+        //     // Menghapus data foto lama supaya berganti baru
+        //     if ($request->oldImage) {
+        //         Storage::delete($request->oldImage);
+        //     }
+        //     $validateData['image'] = $request->file('image')->store('public/post-images');
+        // }
+
         if ($request->file('image')) {
-            // Menghapus data foto lama supaya berganti baru
+            // Hapus gambar lama jika ada
             if ($request->oldImage) {
-                Storage::delete($request->oldImage);
+                Storage::delete('public/' . $request->oldImage);
             }
-            $validateData['image'] = $request->file('image')->store('public/post-images');
+
+            // Simpan file baru dan hanya simpan path tanpa 'public/'
+            $path = $request->file('image')->store('public/post-images');
+            $validateData['image'] = str_replace('public/', '', $path);
         }
 
         // Menyimpan data ke dalamm post
